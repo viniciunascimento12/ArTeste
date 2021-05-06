6989dc8c627f864d887b74e2453d54de3732d53e
Adriel Oliveira <adriel.oliveira@projeto.lead>
2021-05-06 15:51:01 -0300

    3d mockup & stereoscopic view

---
+++ Assets/CameraDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamTexture webCamTexture = new WebCamTexture();
+        this.GetComponent<MeshRenderer>().material.mainTexture = webCamTexture;
+        webCamTexture.Play();        
+    }
+
+}
